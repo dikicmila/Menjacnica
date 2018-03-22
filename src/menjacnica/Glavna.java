817d76dd6f850add133eccc07b85ac20794bd623
dikicmila <mila.dikic97@gmail.com>
2018-03-22 18:47:57 +0100

    Kreirana klasa Glavna koja implementira interfejs

---
+++ src/menjacnica/Glavna.java
@@ -0,0 +1,27 @@
+package menjacnica;
+
+import java.util.GregorianCalendar;
+
+import menjacnica_interfejs.MenjacnicaInterfejs;
+
+public class Glavna implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodajKurs(Valuta valuta, GregorianCalendar datum, String tip, double cena) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs nadjiKurs(Valuta valuta, GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
