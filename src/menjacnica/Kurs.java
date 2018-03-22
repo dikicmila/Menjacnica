06354694fdde1bea7cd336f6f5e8dde7a5bf25cb
dikicmila <mila.dikic97@gmail.com>
2018-03-22 18:37:24 +0100

    Uvedene metode toString, equals i hashCode

--- src/menjacnica/Kurs.java
+++ src/menjacnica/Kurs.java
@@ -3,7 +3,6 @@ package menjacnica;
 import java.util.GregorianCalendar;
 
 public class Kurs {
-
 	private String tip;
 	private GregorianCalendar datum;
 	private double cena;
@@ -25,6 +24,46 @@ public class Kurs {
 	public void setCena(double cena) {
 		this.cena = cena;
 	}
+
+	@Override
+	public String toString() {
+		return "Tip kursa: " + tip + " Datum: " + datum + " Cena: " + cena ;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		long temp;
+		temp = Double.doubleToLongBits(cena);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((tip == null) ? 0 : tip.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Kurs other = (Kurs) obj;
+		if (Double.doubleToLongBits(cena) != Double.doubleToLongBits(other.cena))
+			return false;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (tip == null) {
+			if (other.tip != null)
+				return false;
+		} else if (!tip.equals(other.tip))
+			return false;
+		return true;
+	}
+	
 	
 	
 }
